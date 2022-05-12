5cb4e3b9bf1fa9c2138c4d6ba718ff9cfe1643f6
Tygr <Lung-Yu@users.noreply.github.com>
2022-05-12 20:47:23 +0800

    init inspect project

---
+++ inspectors/src/main/java/com/tygr/inspectors/InspectorsApplication.java
@@ -0,0 +1,13 @@
+package com.tygr.inspectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectorsApplication.class, args);
+	}
+
+}
---
+++ inspectors/src/test/java/com/tygr/inspectors/InspectorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tygr.inspectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
